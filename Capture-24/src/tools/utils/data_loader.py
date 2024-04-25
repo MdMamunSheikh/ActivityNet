e81f598259883d53158a92ec4c902a556fb04e85
Mamun Sheikh <mamunsheikhdueee52@gmail.com>
2024-04-25 12:03:20 +0600

    starting main

--- Capture-24/src/tools/utils/data_loader.py
+++ Capture-24/src/tools/utils/data_loader.py
@@ -1,5 +1,19 @@
+import pandas as pd
+import numpy as np
 import argparse
-import re, os
+import os, re, sys
+from joblib import Parallel, delayed
+from glob import glob
+from tqdm.notebook import tqdm
+from numba import jit
+
+LABELING_STANDARD = ['label:WillettsSpecific2018']
+ANN_PATH = 'annotation-label-dictionary.csv'
+DATAFILES = 'P[0-9][0-9][0-9].csv.gz'
+
+def main(args):
+  os.system(f'mkdir -p {args.output_dir}')
+
 
 def getMET(annotation: str)-> float: # returns MET score from the annotation text
   pattern = re.compile(r'MET\s+(\d+\.\d+)') # Pattern 'MET 1.5'
@@ -12,8 +26,8 @@ def getPID(fname: str)-> str: # returns PID  from a filepath i.e., 'P100'
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument('--datadir', '-d',default='data/', help="Directory containing the raw Capture-24 data. i.e., P001.csv.gz")
-  parser.add_argument('--outdir', '-o',default='prepared_capture24', help="Directory for saving the captured24 processed data")
+  parser.add_argument('--data_dir', '-d',default='data/', help="Directory containing the raw Capture-24 data. i.e., P001.csv.gz")
+  parser.add_argument('--output_dir', '-o',default='prepared_capture24', help="Directory for saving the captured24 processed data")
   parser.add_argument('--win_size', '-w', type=float, default=10, help='Define window size for each segment.')
   parser.add_argument('--FS', '-f', type=int, default=100, help='Define sampling frequency')
   parser.add_argument('--n_jobs', type=int, default=4, help='Number of jobs to load data')
